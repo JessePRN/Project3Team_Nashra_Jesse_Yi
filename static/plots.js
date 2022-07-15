1822a8762107ca44cc8e16bd6fd99cd543f55f91
Jesse Perez <jesseprn@gmail.com>
2022-07-15 14:03:10 -0400

    further changes to stocks and crypto charts working together

--- static/plots.js
+++ static/plots.js
@@ -113,11 +113,25 @@ function stockChangedMultiple() {
   console.log("stock ticker selected value is " + tickerName)
   d3.json("http://127.0.0.1:5000/tickers/name/" + tickerName).then(function (response) {
     // once we get a response, do stuff
-    // console.log("tickerChanged response below");
-    // console.log(response);
+    console.log("tickerChangedMulti response below");
+    console.log(response);
 
     drawTickerMultiple(response)
     buildTable(response)
+  })
+}
+
+function cryptoChangedMultiple() {
+  let dropdownMenu = d3.select("#selCrypto");
+  let tickerName = dropdownMenu.property("value");
+  console.log("crypto ticker selected value is " + tickerName)
+  d3.json("http://127.0.0.1:5000/tickers/name/" + tickerName).then(function (response) {
+    // once we get a response, do stuff
+    console.log("tickerChangedMulti response below");
+    console.log(response);
+
+    drawTickerMultiple(response)
+    buildTable2(response)
   })
 }
 
@@ -237,28 +251,22 @@ function drawTicker(response) {
 init();
 
 function resetLines() {
+  console.log("resetlines executing")
   d3.select('#line').selectAll("*").remove();
+  tickerData = []
 }
 
 // build table
 function buildTable(response) {
   // build a table that displays the latest ticker value and date
   let demoTable = d3.select('#table');
-
-  //clear table before new data is appended
+  //clear table
   demoTable.html('')
-
-  // console.log("table is clear")
-
   //build table
   let fillTable = demoTable.append("mytable")
   let row = fillTable.append('tr')
   let tableData = row.append('td')
-
-  //  console.log("table is built")
-
   // populate table
-
   let Name = tableData.text('For: ' + response[response.length - 1].Name)
   row = fillTable.append('tr')
   tableData = row.append('td')
@@ -268,11 +276,33 @@ function buildTable(response) {
   let Price = tableData.text('Date: ' + response[response.length - 1].Close)
   row = fillTable.append('tr')
   tableData = row.append('td')
+  let Date = tableData.text('Close: ' + response[response.length - 1].Date)
+  row = fillTable.append('tr')
+  tableData = row.append('td')
+}
+
+function buildTable2(response) {
+  // build a table that displays the latest ticker value and date
+  let demoTable = d3.select('#table2');
+  //clear table
+  demoTable.html('')
+  //build table
+  let fillTable = demoTable.append("mytable")
+  let row = fillTable.append('tr')
+  let tableData = row.append('td')
+  // populate table
+  let Name = tableData.text('For: ' + response[response.length - 1].Name)
+  row = fillTable.append('tr')
+  tableData = row.append('td')
+  let Ticker = tableData.text('Ticker: ' + response[response.length - 1].Ticker)
+  row = fillTable.append('tr')
+  tableData = row.append('td')
+  let Price = tableData.text('Close: ' + response[response.length - 1].Close)
+  row = fillTable.append('tr')
+  tableData = row.append('td')
   let Date = tableData.text('Date: ' + response[response.length - 1].Date)
   row = fillTable.append('tr')
   tableData = row.append('td')
-
-  // console.log("table is being filled")
 }
 
 //populates date selector for bubble chart
